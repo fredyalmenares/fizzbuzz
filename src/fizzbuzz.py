f28ea2fde19f088bada425495338bc2e57971c6b
Fredy <fredyalmenares@gmail.com>
2020-12-04 17:31:17 -0500

    Function calculate_fizzbuzz finished. Added type annotation to all FizzBuzz functions.

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,6 +1,17 @@
-def calculate_fizzbuzz(number):
-    return
+from typing import Union
 
 
-def calculate_fizzbuzz_range(start, end):
-    return
+def calculate_fizzbuzz(number: int) -> Union[str, int]:
+    """ Determines the output of the FizzBuzz method for a given number """
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
+    elif number % 3 == 0:
+        return 'Fizz'
+    elif number % 5 == 0:
+        return 'Buzz'
+    else:
+        return number
+
+
+def calculate_fizzbuzz_range(start: int, end: int) -> list:
+    return []
